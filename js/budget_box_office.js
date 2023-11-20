aac559e6776c8f2ad0dbc1984704b3539d1e9794
tfargiano <112838530+tfargiano@users.noreply.github.com>
2023-11-20 11:47:30 -0500

    Added budget data + budget/box office js

---
+++ js/budget_box_office.js
@@ -0,0 +1,129 @@
+class RankChart {
+
+    constructor(_parentElement, data1, data2, data3) {
+        this.parentElement = _parentElement;
+        this.budgetdata = data1
+        this.imdbdata = data2
+        this.data = data3
+
+        this.initVis();
+    }
+
+    initVis() {
+        // Method to initialize the visualization
+        let vis = this;
+
+        vis.margin = { top: 40, right: 0, bottom: 60, left: 60 };
+
+        vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
+        vis.height = 300 - vis.margin.top - vis.margin.bottom;
+
+        // SVG drawing area
+        vis.svg = d3.select("#" + vis.parentElement).append("svg")
+            .attr("width", vis.width + vis.margin.left + vis.margin.right)
+            .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
+
+        // clip path
+        vis.svg.append('g')
+            .attr('class', 'title')
+            .append('text')
+            .text('Box Office Performance of Winners')
+            .attr('transform', `translate(${vis.width / 2}, 20)`)
+            .attr('text-anchor', 'middle');
+
+        // init scales
+        vis.xScale = d3.scaleLinear()
+            .domain([d3.min(vis.data, d => d.Year), d3.max(vis.data, d => d.Year)])
+            .range([0, vis.width]);
+
+        vis.yScale = d3.scaleLinear()
+            .domain([1, 10])
+            .range([vis.height, 0]);
+
+        // init x & y axis
+        vis.xAxis = vis.svg.append("g")
+            .attr("class", "axis axis--x")
+            .attr("transform", "translate(0," + vis.height + ")");
+        vis.yAxis = vis.svg.append("g")
+            .attr("class", "axis axis--y");
+
+        vis.BOline = d3.line()
+            .x(d => vis.xScale(d.Year))
+            .y(d => vis.yScale(d.BoxOffice_Rank));
+
+        vis.BUline = d3.line()
+            .x(d => vis.xScale(d.Year))
+            .y(d => vis.yScale(d.Budget_Rank));
+
+        // Filter data to include only winners
+
+
+        // // init pathGroup
+        // vis.pathGroup = vis.svg.append('g').attr('class', 'pathGroup');
+        //
+        // // init path one (average)
+        // vis.pathOne = vis.pathGroup
+        //     .append('path')
+        //     .attr("class", "pathOne");
+        //
+        // // init path two (single state)
+        // vis.pathTwo = vis.pathGroup
+        //     .append('path')
+        //     .attr("class", "pathTwo");
+
+
+        vis.wrangleData();
+    }
+
+    wrangleData() {
+        // Method to wrangle the data
+        let vis = this;
+        console.log(vis.data);
+        vis.winnersData = vis.data.filter(d => d.winner.toLowerCase() === "true");
+
+        // vis.winnersData = vis.data.filter(d => d.winner === true);
+
+        // console.log(vis.data);
+        // console.log(vis.budgetdata);
+        // vis.winnersData = vis.data.filter(d => d.true);
+        console.log(vis.winnersData);
+        vis.filteredWinnersData = vis.winnersData.filter(d => !isNaN(d.BoxOffice_Rank) && !isNaN(d.Budget_Rank));
+
+
+        console.log(vis.filteredWinnersData)
+
+
+        vis.updateVis();
+    }
+
+    updateVis() {
+        // Method to update the visualization
+        let vis = this;
+
+        vis.xAxis.call(d3.axisBottom(vis.xScale));
+        vis.yAxis.call(d3.axisLeft(vis.yScale))
+
+        // Bind data to line elements
+        vis.svg.selectAll('.boxoffice-line')
+            .data([vis.filteredWinnersData])
+            .enter()
+            .append('path')
+            .attr('class', 'boxoffice-line')
+            .attr('d', vis.BOline)
+            .attr('fill', 'none')
+            .attr('stroke', 'steelblue');
+        // console.log("data updated");
+
+        vis.svg.selectAll('.budget-line')
+            .data([vis.filteredWinnersData])
+            .enter()
+            .append('path')
+            .attr('class', 'budget-line')
+            .attr('d', vis.BUline)
+            .attr('fill', 'none')
+            .attr('stroke', 'gray');
+        // console.log("data updated");
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
+
 // (1) Load data with promises
 
 let promises = [
     d3.json("data/movies_list.json"),
-    d3.csv("data/oscars_df.csv")
+    d3.csv("data/oscars_df.csv"),
+    d3.csv("data/film_budget_data.csv"),
+    d3.json("data/cleanerdata.json")
 ];
 
 Promise.all(promises)
@@ -16,9 +19,12 @@ Promise.all(promises)
 function createVis(data) {    
     let movieList = data[0];
     let oscarsDF = data[1];
+    let budgetData = data[2]
+    let squeakyCleanData = data[3]
 
     console.log(data);
     // Create visualization instances
-    let hemisphere = new Hemisphere("hemisphere");
+    // let hemisphere = new Hemisphere("hemisphere");
+    let rankchart = new RankChart("rankchart", budgetData, movieList, squeakyCleanData);
 
 }
