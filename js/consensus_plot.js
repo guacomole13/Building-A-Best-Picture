80941ad6948699bde9cd53d9a79f1b18e7a45e4a
lwu595 <129014173+lwu595@users.noreply.github.com>
2023-12-06 17:17:29 -0500

    added TODO comment

--- js/consensus_plot.js
+++ js/consensus_plot.js
@@ -225,11 +225,13 @@ class ConsensusPlot {
             .attr("width", 20)
             .attr("height", 20);
 
-        // Append text label for rotten tomatoes audience rating
+        // Append text label for rotten audience rating
         legend.append("text")
             .attr("x", 35) // Adjust label position relative to the star image
             .attr("y", 155) // Adjust label position relative to the star image
             .text("Rotten Audience Rating (<60%)");
+
+        // TODO: ADD TIMELINE BRUSHING
 
     }
 }
